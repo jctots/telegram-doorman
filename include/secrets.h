084e444e61d7a70c55f2d47d1da2eea9d7411a56
jctots <jctots@pm.me>
2024-06-25 21:25:07 +0200

    add dummy secrets.h

---
+++ include/secrets.h
@@ -0,0 +1,22 @@
+#pragma once
+// Wifi network station credentials
+#define DEFAULT_WIFI_SSID "xxxyyyzzz"
+#define DEFAULT_WIFI_PASSWORD "xxxyyyzzz"
+// Telegram BOT Token (Get from Botfather)
+#define BOT_TOKEN "xxxyyyzzz:xxxyyyzzz"
+
+// Use @myidbot (IDBot) to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+#define DEFAULT_TELEGRAM_CHAT_ID "xxxyyyzzz"
+// for adding/removing new bot users
+#define BOT_USERS_PASSWORD "xxxyyyzzz"
+
+
+#define DEFAULT_TCS_APT_BELL 0xDEADBEEF
+#define DEFAULT_TCS_STREET_CALL 0xDEADBEEF
+#define DEFAULT_TCS_GARAGE_CALL 0xDEADBEEF
+#define DEFAULT_TCS_STREET_VIEW 0xDEADBEEF
+#define DEFAULT_TCS_GARAGE_VIEW 0xDEADBEEF
+#define DEFAULT_TCS_STREET_OPEN 0xDEADBEEF
+#define DEFAULT_TCS_GARAGE_OPEN 0xDEADBEEF
