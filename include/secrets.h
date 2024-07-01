3e04446965952c10022ca5744e694ccff6f13d62
jctots <jctots@pm.me>
2024-07-01 23:52:03 +0200

    Develop (#2)
    * loop handling, password check, format request
    
    * update readme

--- include/secrets.h
+++ include/secrets.h
@@ -2,6 +2,8 @@
 // Wifi network station credentials
 #define DEFAULT_WIFI_SSID "xxxyyyzzz"
 #define DEFAULT_WIFI_PASSWORD "xxxyyyzzz"
+#define DEFAULT_OTA_PASSWORD "xxxyyyzzz"
+
 // Telegram BOT Token (Get from Botfather)
 #define BOT_TOKEN "xxxyyyzzz:xxxyyyzzz"
 
@@ -9,9 +11,9 @@
 // Also note that you need to click "start" on a bot before it can
 // message you
 #define DEFAULT_TELEGRAM_CHAT_ID "xxxyyyzzz"
-// for adding/removing new bot users
-#define BOT_USERS_PASSWORD "xxxyyyzzz"
-#define OTA_PASSWORD "xxxyyyzzz"
+// for adding/removing new bot users (propose to add '/' at the beginning so bot will not ignore in 'privacy mode')
+#define DEFAULT_TELEGRAM_PASSWORD "/xxxyyyzzz"
+
 
 #define DEFAULT_TCS_APT_BELL 0xDEADBEEF
 #define DEFAULT_TCS_STREET_CALL 0xDEADBEEF
--- src/dataHandler.cpp
+++ src/dataHandler.cpp
@@ -21,9 +21,11 @@ void saveConfig(Config &config)
     // Set the values in the document
     doc["wifiSsid"] = config.wifi.ssid;
     doc["wifiPassword"] = config.wifi.password;
-    doc["telegramChatIdMain"] = config.telegram.chatIdMain;
-    doc["telegramChatIdSub1"] = config.telegram.chatIdSub1;
-    doc["telegramChatIdSub2"] = config.telegram.chatIdSub2;
+    doc["wifiOtaPass"] = config.wifi.otaPass;
+    doc["telegramChatId0"] = config.telegram.chatId0;
+    doc["telegramChatId1"] = config.telegram.chatId1;
+    doc["telegramChatId2"] = config.telegram.chatId2;
+    doc["telegramPassword"] = config.telegram.password;
     doc["tcsApartmentBell"] = config.tcs.apartmentBell;
     doc["tcsStreetCall"] = config.tcs.streetCall;
     doc["tcsGarageCall"] = config.tcs.garageCall; 
@@ -32,14 +34,14 @@ void saveConfig(Config &config)
     doc["tcsStreetOpen"] = config.tcs.streetOpen; 
     doc["tcsGarageOpen"] = config.tcs.garageOpen; 
     doc["partyMode"] = config.partyMode;
-    doc["silentModeMain"] = config.silentModeMain; 
-    doc["silentModeSub1"] = config.silentModeSub1; 
-    doc["silentModeSub2"] = config.silentModeSub2; 
+    doc["silentMode0"] = config.silentMode0; 
+    doc["silentMode1"] = config.silentMode1; 
+    doc["silentMode2"] = config.silentMode2; 
     doc["sniffMode"] = config.sniffMode; 
     doc["devMode"] = config.devMode; 
-    doc["silentModeMainPrev"] = config.silentModeMainPrev; 
-    doc["silentModeSub1Prev"] = config.silentModeSub1Prev; 
-    doc["silentModeSub2Prev"] = config.silentModeSub2Prev; 
+    doc["silentMode0Prev"] = config.silentMode0Prev; 
+    doc["silentMode1Prev"] = config.silentMode1Prev; 
+    doc["silentMode2Prev"] = config.silentMode2Prev; 
     doc["sniffModePrev"] = config.sniffModePrev; 
 
  
@@ -80,10 +82,12 @@ bool loadConfig(Config &config)
         // load wifi config
         if (doc.containsKey("wifiSsid")) strncpy(config.wifi.ssid, doc["wifiSsid"].as<const char *>(), sizeof(config.wifi.ssid));
         if (doc.containsKey("wifiPassword")) strncpy(config.wifi.password, doc["wifiPassword"].as<const char *>(), sizeof(config.wifi.password));
+        if (doc.containsKey("wifiOtaPass")) strncpy(config.wifi.otaPass, doc["wifiOtaPass"].as<const char *>(), sizeof(config.wifi.otaPass));
         // load telegram config
-        if (doc.containsKey("telegramChatIdMain")) strncpy(config.telegram.chatIdMain, doc["telegramChatIdMain"].as<const char *>(), sizeof(config.telegram.chatIdMain));
-        if (doc.containsKey("telegramChatIdSub1")) strncpy(config.telegram.chatIdSub1, doc["telegramChatIdSub1"].as<const char *>(), sizeof(config.telegram.chatIdSub1));
-        if (doc.containsKey("telegramChatIdSub2")) strncpy(config.telegram.chatIdSub2, doc["telegramChatIdSub2"].as<const char *>(), sizeof(config.telegram.chatIdSub2));
+        if (doc.containsKey("telegramChatId0")) strncpy(config.telegram.chatId0, doc["telegramChatId0"].as<const char *>(), sizeof(config.telegram.chatId0));
+        if (doc.containsKey("telegramChatId1")) strncpy(config.telegram.chatId1, doc["telegramChatId1"].as<const char *>(), sizeof(config.telegram.chatId1));
+        if (doc.containsKey("telegramChatId2")) strncpy(config.telegram.chatId2, doc["telegramChatId2"].as<const char *>(), sizeof(config.telegram.chatId2));
+        if (doc.containsKey("telegramPassword")) strncpy(config.telegram.password, doc["telegramPassword"].as<const char *>(), sizeof(config.telegram.password));
         // load tcs config
         if (doc.containsKey("tcsApartmentBell")) config.tcs.apartmentBell = doc["tcsApartmentBell"];
         if (doc.containsKey("tcsStreetCall")) config.tcs.streetCall = doc["tcsStreetCall"];
@@ -94,14 +98,14 @@ bool loadConfig(Config &config)
         if (doc.containsKey("tcsGarageOpen")) config.tcs.garageOpen = doc["tcsGarageOpen"];
         // load bot config
         if (doc.containsKey("partyMode")) config.partyMode = doc["partyMode"];
-        if (doc.containsKey("silentModeMain")) config.silentModeMain = doc["silentModeMain"];
-        if (doc.containsKey("silentModeSub1")) config.silentModeSub1 = doc["silentModeSub1"];
-        if (doc.containsKey("silentModeSub2")) config.silentModeSub2 = doc["silentModeSub2"];
+        if (doc.containsKey("silentMode0")) config.silentMode0 = doc["silentMode0"];
+        if (doc.containsKey("silentMode1")) config.silentMode1 = doc["silentMode1"];
+        if (doc.containsKey("silentMode2")) config.silentMode2 = doc["silentMode2"];
         if (doc.containsKey("sniffMode")) config.sniffMode = doc["sniffMode"];
         if (doc.containsKey("devMode")) config.devMode = doc["devMode"];
-        if (doc.containsKey("silentModeMainPrev")) config.silentModeMainPrev = doc["silentModeMainPrev"];
-        if (doc.containsKey("silentModeSub1Prev")) config.silentModeSub1Prev = doc["silentModeSub1Prev"];
-        if (doc.containsKey("silentModeSub2Prev")) config.silentModeSub2Prev = doc["silentModeSub2Prev"];
+        if (doc.containsKey("silentMode0Prev")) config.silentMode0Prev = doc["silentMode0Prev"];
+        if (doc.containsKey("silentMode1Prev")) config.silentMode1Prev = doc["silentMode1Prev"];
+        if (doc.containsKey("silentMode2Prev")) config.silentMode2Prev = doc["silentMode2Prev"];
         if (doc.containsKey("sniffModePrev")) config.sniffModePrev = doc["sniffModePrev"];
         // load successful
         return_error = 0;
@@ -122,6 +126,9 @@ void setupConfig(void)
     
     bool error = loadConfig(config);
     if (error){
+        LittleFS.end();
+        LittleFS.format();
+        LittleFS.begin();
         saveConfig(config);  // save defaults if cannot load
         Serial.println("Default config saved");
     }
@@ -129,9 +136,11 @@ void setupConfig(void)
     //for debugging
     Serial.print("w1): ");   Serial.println(config.wifi.ssid);
     Serial.print("w2): ");   Serial.println(config.wifi.password);
-    Serial.print("t1): ");   Serial.println(config.telegram.chatIdMain);
-    Serial.print("t2): ");   Serial.println(config.telegram.chatIdSub1);
-    Serial.print("t3): ");   Serial.println(config.telegram.chatIdSub2);
+    Serial.print("w3): ");   Serial.println(config.wifi.otaPass);
+    Serial.print("t1): ");   Serial.println(config.telegram.chatId0);
+    Serial.print("t2): ");   Serial.println(config.telegram.chatId1);
+    Serial.print("t3): ");   Serial.println(config.telegram.chatId2);
+    Serial.print("t4): ");   Serial.println(config.telegram.password);
     Serial.print("b1): ");   Serial.println(config.tcs.apartmentBell, HEX);
     Serial.print("b2): ");   Serial.println(config.tcs.streetCall, HEX);
     Serial.print("b3): ");   Serial.println(config.tcs.garageCall, HEX);
@@ -140,14 +149,14 @@ void setupConfig(void)
     Serial.print("b6): ");   Serial.println(config.tcs.streetOpen, HEX);
     Serial.print("b7): ");   Serial.println(config.tcs.garageOpen, HEX);
     Serial.print("m1): ");   Serial.println(config.partyMode);
-    Serial.print("m2): ");   Serial.println(config.silentModeMain);
-    Serial.print("m3): ");   Serial.println(config.silentModeSub1);
-    Serial.print("m4): ");   Serial.println(config.silentModeSub2);
+    Serial.print("m2): ");   Serial.println(config.silentMode0);
+    Serial.print("m3): ");   Serial.println(config.silentMode1);
+    Serial.print("m4): ");   Serial.println(config.silentMode2);
     Serial.print("m5): ");   Serial.println(config.sniffMode);
     Serial.print("m6): ");   Serial.println(config.devMode);
-    Serial.print("m7): ");   Serial.println(config.silentModeMainPrev);
-    Serial.print("m8): ");   Serial.println(config.silentModeSub1Prev);
-    Serial.print("m9): ");   Serial.println(config.silentModeSub2Prev);
+    Serial.print("m7): ");   Serial.println(config.silentMode0Prev);
+    Serial.print("m8): ");   Serial.println(config.silentMode1Prev);
+    Serial.print("m9): ");   Serial.println(config.silentMode2Prev);
     Serial.print("mA): ");   Serial.println(config.sniffModePrev);
 
 }
